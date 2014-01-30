9d03dcb9e895fe32aae29abeed653d5a35ad4db8
Yojimbo <pypointereau+web@gmail.com>
2014-01-30 18:26:40 +1100

    Basic file setup

---
+++ src/js/progressify.jquery.js
@@ -0,0 +1,17 @@
+(function( $ ) {
+
+	// Plugin declaration
+	$.fn.progressify = function( options ) {
+
+
+
+
+		// Each to loop on matching element + Return to maintain chainability
+		return this.each(function(index) {
+			console.log('Bar found', index)
+			
+		});
+
+	};
+
+}( jQuery ));
