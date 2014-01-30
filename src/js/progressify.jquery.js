66c3d9eeaf0472410b9135ed6bf89d7b822303ac
Yojimbo <pypointereau+web@gmail.com>
2014-01-30 19:06:41 +1100

    JS: Adding event on the control panel

--- src/js/progressify.jquery.js
+++ src/js/progressify.jquery.js
@@ -11,6 +11,42 @@
 		// Declaring the currently selected bar
 		var currentBar;
 
+		// Binding change action on the <select>
+		$select.on('change', function(){
+			var $this = $(this);
+
+			// We change currentBar value to the one selected
+			currentBar = $this.find(":selected").val();
+		});
+
+
+		// Binding click action on <button>
+		$buttons.on('click', function(){
+			var $this = $(this),
+				$currentBar = $('#'+currentBar),
+				newWidth;
+
+			// Calculating the new width
+			newWidth = Number($currentBar.data('width')) + Number($this.data('amount'));
+			
+			// Limit it to 0 if negative
+			if(newWidth < 0){
+				newWidth = 0;
+			}
+
+			// Adding / removing a class if it's over / under 100%
+			if(newWidth > 100){
+				$currentBar.find('div').addClass('over100');
+			}else{
+				$currentBar.find('div').removeClass('over100');
+			}
+			
+			// Applying changes to the DOM (Width, data-attribute and text)
+			$currentBar.data('width', newWidth).find('span').text(newWidth+'%');
+			$('div', $currentBar).width(newWidth+'%');
+
+		});
+
 
 		// Each to loop on matching element + Return to maintain chainability
 		return this.each(function() {
